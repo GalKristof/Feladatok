fc35223bf700d64cfa56bd8a935f98102e2bb686
Gál Kristóf <gal.kristof08@gmail.com>
2022-07-10 19:51:15 +0200

    1. feladat összes hibájának kijavítása

---
+++ Feladat1/script.js
@@ -0,0 +1,12 @@
+function toggleEye()
+{
+    var password = document.getElementById("password");
+    if(password.type === "password")
+    {
+        password.type = "text";
+    }
+    else
+    {
+        password.type = "password";    
+    }
+}
